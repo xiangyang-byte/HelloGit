6e7153de4a16a31a1439d57f63471407686aeb7c
xiangyang-byte <1621714941@qq.com>
2019-11-13 09:14:05 +0800

    66

--- src/com/atguigu/Hello/hello.java
+++ src/com/atguigu/Hello/hello.java
@@ -3,8 +3,7 @@ package com.atguigu.Hello;
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("1111111");
-		System.out.println("11111");
+		System.out.println("6");
 
 	}
 
